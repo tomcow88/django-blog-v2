221af9938bf928c7d0d43f6245e060528701f590
Tom Cowen <tomreescowen@gmail.com>
2024-03-10 23:20:57 +0000

    Add excerpt and updated on to Post model

--- blog/models.py
+++ blog/models.py
@@ -12,5 +12,7 @@ class Post(models.Model):
         User, on_delete=models.CASCADE, related_name="blog_posts"
     )
     content = models.TextField()
+    excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
