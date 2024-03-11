6aea73a0fedf4baa78e863d2c8e110dd8d5262df
Tom Cowen <tomreescowen@gmail.com>
2024-03-11 21:41:06 +0000

    add custom images to post and about models

--- about/models.py
+++ about/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
@@ -6,6 +7,7 @@ class About(models.Model):
     title = models.CharField(max_length=200, unique=True)
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
+    profile_image = CloudinaryField('image', default='placeholder')
 
     def __str__(self):
         return self.title
