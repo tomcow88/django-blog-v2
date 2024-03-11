affb5d3a15a64e05505ef9d0b4944d1500016399
Tom Cowen <tomreescowen@gmail.com>
2024-03-11 00:03:15 +0000

    Add methods to post and comment models.

--- blog/models.py
+++ blog/models.py
@@ -17,6 +17,12 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
 
+    class Meta:
+        ordering = ["-created_on"]
+        
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
+
 
 class Comment(models.Model):
     post = models.ForeignKey(
@@ -28,3 +34,9 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
